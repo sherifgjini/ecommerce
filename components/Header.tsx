113366c3f372572d93f958d7a2b9904b8728e117
Sherif <sherif@gjirafa.com>
2023-11-09 22:08:01 +0100

    Cache added

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,8 +8,8 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = () => {
     return (  
         <header className="mt-5 h-16 rounded-xl bg-primary flex justify-between items-center px-6 sticky top-4 shadow-xl">
-            <div className="w-[152px]">Logo</div>
-            <Input className="w-80" placeholder="Search" icon />
+            <div className="w-[152px] text-white">Logo</div>
+            {/* <Input className="w-80" placeholder="Search" icon /> */}
             <Content />
         </header>
     );
--- services/GetProducts.service.ts
+++ services/GetProducts.service.ts
@@ -1,7 +1,7 @@
-import {  IProducts } from "@/interfaces/IProducts.types"
+import { IProducts } from "@/interfaces/IProducts.types"
 
 export const getProcuts = async (): Promise<IProducts> => {
-    const response = await fetch("https://api.npoint.io/395be385795b6dc5391d");
+    const response = await fetch("https://api.npoint.io/a09ac7b91bd15293ca54", {next: {revalidate: 3600}});
     if (response.status !== 200 ) {
         throw new Error('Failed to fetch data')
     }
